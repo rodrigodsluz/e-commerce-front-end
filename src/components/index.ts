692697ce8c0c51846aab9a4e317c365bade31e31
rodrigodsluz <rodrigodsluz@gmail.com>
2021-01-18 15:40:29 -0300

    Fax

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as Layout } from './Layout';
-export { default as Card } from './Card';
-export { default as Checkbox } from './Checkbox';

567d1f5e10f458c886977d34054f0f6d564b231a
rodrigodsluz <rodrigodsluz@gmail.com>
2021-01-18 13:33:28 -0300

    Fix

--- src/pages/admin/orders/index.tsx
+++ src/pages/admin/orders/index.tsx
@@ -87,7 +87,7 @@ const Orders = () => {
   return (
     <Layout
       title="Orders"
-      description={`G'day ${user.name}, you can manage all the orders here`}
+      description={"G'day aa, you can manage all the orders here"}
       className="container-fluid"
     >
       <div className="row">
